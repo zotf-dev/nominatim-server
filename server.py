b798968cfe01e98fdb0d7e24cce9cc71411051b4
zotf-dev <zotf124@gmail.com>
2025-10-16 19:42:23 +0300

    added iso

--- server.py
+++ server.py
@@ -4,17 +4,17 @@ import requests
 
 app = FastAPI(title="Address Lookup API", version="1.0")
 
-def get_real_addresses(address: str):
+def get_real_addresses(address: str, iso: str):
     """
     Fetch real addresses from OpenStreetMap (Nominatim API).
     Returns a list of display names or an error message.
     """
     try:
         url = "https://nominatim.openstreetmap.org/search"
-        params = {"q": address, "format": "json", "limit": 100, "accept-language": "en"}
+        params = {"q": address, "format": "json", "limit": 1, "accept-language": "en", "countrycodes": iso}
         headers = {"User-Agent": "address-checker"}  # Required by Nominatim
         
-        response = requests.get(url, params=params, headers=headers, timeout=5)
+        response = requests.get(url, params=params, headers=headers, timeout=3)
         response.raise_for_status()
         
         data = response.json()
@@ -22,15 +22,17 @@ def get_real_addresses(address: str):
         return display_names
 
     except requests.exceptions.Timeout:
-        return ["TIMEOUT"]
+        print('TIMEOUT')
+        return []
     except requests.exceptions.RequestException as e:
-        return [f"ERROR: {e}"]
+        print(e)
+        return []
 
 @app.get("/get_addresses")
-def get_addresses(address: str = Query(..., description="Address to search for")):
+def get_addresses(address: str = Query(..., description="Address to search for"), iso: str = ""):
     """
     Get address variations from Nominatim.
     Example: /get_addresses?address=Baghdad
     """
-    results = get_real_addresses(address)
+    results = get_real_addresses(address, iso)
     return JSONResponse(content={"address": address, "results": results})
